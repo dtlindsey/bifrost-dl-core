c859fc469dc7c6c35faaeede4de1baf27051157a
Daniel Lindsey <dtlindsey@gmail.com>
2021-07-30 00:12:53 +0900

    group list widgets working

--- maya/python/dl_bifrost_utils/ui/dl_group_manager_ui.py
+++ maya/python/dl_bifrost_utils/ui/dl_group_manager_ui.py
@@ -3,11 +3,14 @@ from PySide2 import QtWidgets
 
 from . import maya_main_window
 
+from maya import cmds
 
 class GroupManager(QtWidgets.QDialog):
     def __init__(self, parent=maya_main_window.get_maya_instance()):
         super(GroupManager, self).__init__(parent)
-
+        # data
+        self._first_set_items = [] # stores the set info of the selected group in first_list
+        #
         self.setWindowTitle("DL Group Manager")
         self.setMinimumWidth(300)
         self.setMinimumHeight(500)
@@ -32,9 +35,16 @@ class GroupManager(QtWidgets.QDialog):
     def _create_widgets(self):
         # first groups
         self._first_group_list = QtWidgets.QListWidget()
+        self._first_group_list.setSortingEnabled(True)
+        self._first_group_list.clicked.connect(self._first_group_select)
 
         # second groups
         self._second_group_list = QtWidgets.QListWidget()
+        self._second_group_list.setSortingEnabled(True)
+        self._second_group_list.clicked.connect(self._second_group_select)
+
+        # build the list
+        self._list_all_selection_sets()
 
         # new group button
         self._new_group_button = QtWidgets.QPushButton(text="New Group")
@@ -85,4 +95,42 @@ class GroupManager(QtWidgets.QDialog):
         print("remove elements from selected group.")
 
     def _list_all_selection_sets(self):
-        print("Get selection sets")
+        """
+        This creates the list data in of teh groups
+        """
+        default_sets = ['defaultLightSet', 'defaultObjectSet', 'initialParticleSE', 'initialShadingGroup']
+        selection_sets = cmds.ls(type="objectSet")
+        # remove the defaults
+        for default_set in default_sets:
+            selection_sets.remove(default_set)
+        #
+        self._first_group_list.addItems(selection_sets)
+        self._second_group_list.addItems(selection_sets)
+
+    def _first_group_select(self):
+        #
+        q_sel_group_items = self._first_group_list.selectedItems()
+        sel_group_items = [q_sel_group_items[0].text()]
+        self._first_set_items = cmds.sets(sel_group_items, query=True)
+        self.select_set_items(self._first_set_items)
+
+    def _second_group_select(self):
+        #
+        q_sel_group_items = self._second_group_list.selectedItems()
+        if q_sel_group_items:
+            sel_group_items = []
+            # need to convert the data to strings to be able to use in maya.cmds
+            for q_item in q_sel_group_items:
+                sel_group_items.append(q_item.text())
+            #
+            set_items = cmds.sets(sel_group_items, query=True)
+            if self._first_set_items:
+                set_items += self._first_set_items
+            self.select_set_items(set_items)
+        else:
+            self.select_set_items(self._first_set_items)
+
+    @staticmethod
+    def select_set_items(set_items):
+        cmds.select(clear=True)
+        cmds.select(set_items)
